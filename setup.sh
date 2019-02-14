017bd2c50400e79cf2c5b5d6ba89fc0a4f4e21b3
Prakash Thapa <p.thapa@insign.ch>
2019-02-14 10:46:30 +0100

    add php7.1

--- setup.sh
+++ setup.sh
@@ -5,9 +5,9 @@ export DEBIAN_FRONTEND=noninteractive
 locale-gen en en_US en_US.UTF-8
 dpkg-reconfigure locales
 
-export LC_ALL='en_US.UTF-8'
-export LANG='en_US.UTF-8'
-export LANGUAGE='en_US.UTF-8'
+export LC_ALL='C.UTF-8'
+export LANG='C.UTF-8'
+export LANGUAGE='C.UTF-8'
 
 LC_ALL=C.UTF-8 add-apt-repository -y ppa:ondrej/php 
 apt-get -qy update
