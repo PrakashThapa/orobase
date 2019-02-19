7b9d705020694b6547a07919a6c7d847a088a070
Prakash Thapa <p.thapa@insign.ch>
2019-02-19 21:45:31 +0100

    ..

--- setup.sh
+++ setup.sh
@@ -51,6 +51,7 @@ runuser -s /bin/sh -c 'composer global require fxp/composer-asset-plugin:1.4.4' 
 
 # Install node.js
 apt-get purge nodejs*
+apt-get clean && apt-get autoremove && apt-get install -f
 curl -sL https://deb.nodesource.com/setup_8.x |  bash -
 apt-get install nodejs -y && apt-get install npm -y  || exit 1
 
