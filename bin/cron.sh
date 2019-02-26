dc26ca0d420ed6d8d8f121b02d375e56c35fae7c
Prakash Thapa <p.thapa@insign.ch>
2019-02-26 08:18:09 +0100

    update bin/ directory

--- bin/cron.sh
+++ bin/cron.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 /usr/local/bin/waitinstall.sh
-exec /usr/local/bin/listener.php /var/log/oro-cron.log /var/www/app/console --env=prod oro:cron
+exec /usr/local/bin/listener.php /var/log/oro-cron.log /var/www/bin/console --env=prod oro:cron
